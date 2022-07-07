1980b5dba6373d6a6256fe5a0aac1ec8297b5811
aimonimran <aimon.imran@gmail.com>
2022-07-07 21:21:00 +0500

    HTTP POST request

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     {id: 1, name: 'course1'},
@@ -20,6 +23,26 @@ app.get('/api/courses/:id', (req, res) => {
     if (!course) res.status(404).send('Course with the given ID was not found!');
     res.send(course);
 })
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
