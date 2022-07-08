7315897c32b70703fb4fbac32f089754e1aa1af0
aimonimran <aimon.imran@gmail.com>
2022-07-08 19:13:25 +0500

    Used templating engine - Pug

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const logger = require('./logger');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -31,7 +34,8 @@ const courses = [
 ];
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index', {title: 'My Express App', message: 'Hello'});
+    // res.send('Hello');
 })
 
 app.get('/api/courses', (req, res) => {
@@ -86,5 +90,5 @@ app.delete('/api/courses/:id', (req, res) => {
     res.send(course);
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
